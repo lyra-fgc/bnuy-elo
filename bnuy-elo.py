371778d0954bd5447ef9c489d56a40f9d6a73957
GSabelette <asus@BERNARDO.localdomain>
2023-02-15 00:33:00 +0100

    Does not account for forfeited matches now. Also fixed non checked-in players getting added to the sheet.

--- bnuy-elo.py
+++ bnuy-elo.py
@@ -44,40 +44,52 @@ for p in participants :
 matches = challonge.matches.index(tournament["id"])
 
 # We will store the matches in a per-participant basis to facilitate ELO calculation
-# participant_matches[user_id] = [[win_status, other_user_id]]
+# participant_matches[user_id] = [{"win_status", "opponent_id", "forfeited"}]
 participants_matches = {}
 for id in challonge_user_id_dict :
     participants_matches[id] = []
 
 # Fill the per-participant matches
 for match in matches : 
-    participants_matches[match["winner_id"]].append([1, match["loser_id"]])
-    participants_matches[match["loser_id"]].append([0, match["winner_id"]])
-
+    winner_dict = {}
+    loser_dict = {}
+    
+    winner_dict["win_status"] = 1
+    winner_dict["opponent_id"] = match["loser_id"]
+    winner_dict["forfeited"] = match["forfeited"]
+    
+    loser_dict["win_status"] = 0
+    loser_dict["opponent_id"] = match["winner_id"]
+    loser_dict["forfeited"] = match["forfeited"]
+    
+    participants_matches[match["winner_id"]].append(winner_dict)
+    participants_matches[match["loser_id"]].append(loser_dict)
+    
+# Compute the new ELO values and store them in a dict
 new_elo_dict = {}
-
-# Compute the new ELO values
-for participant in participants_matches :
-    challonge_id = challonge_user_id_dict[participant]
-    player_name = names_dict.get(challonge_id)
-    sheet_elo = elo_dict.get(player_name)
-    player_elo = sheet_details["elo_base_value"]
-    if sheet_elo :
-        player_elo = sheet_elo
-    else : 
-        print("Adding new user", challonge_id, player_name, "with baseline ELO", sheet_details["elo_base_value"])
-    new_elo = player_elo
-    for match in participants_matches[participant] : 
-        win_status = match[0]
-        other_name = names_dict.get(challonge_user_id_dict[match[1]])
-        other_elo = sheet_details["elo_base_value"]
-        other_sheet_elo = elo_dict.get(other_name)
-        if other_sheet_elo : 
-            other_elo = other_sheet_elo
-        new_elo += sheet_details["elo_k_factor"] * (win_status - 1/(1+pow(10, (other_elo-player_elo)/400)))
-    new_elo = round(new_elo)
-    new_elo_dict[player_name] = new_elo
-    print(player_name, "old elo :", player_elo, "new_elo :", new_elo)
+for participant in participants_matches :   
+    if (len(participants_matches[participant]) != 0) :      
+        challonge_id = challonge_user_id_dict[participant]
+        player_name = names_dict.get(challonge_id)
+        sheet_elo = elo_dict.get(player_name)
+        player_elo = sheet_details["elo_base_value"]
+        if sheet_elo :
+            player_elo = sheet_elo
+        else : 
+            print("Adding new user", challonge_id, player_name, "with baseline ELO", sheet_details["elo_base_value"])
+        new_elo = player_elo
+        for match in participants_matches[participant] : 
+            if (match["forfeited"]) != True : 
+                win_status = match["win_status"]
+                other_name = names_dict.get(challonge_user_id_dict[match["opponent_id"]])
+                other_elo = sheet_details["elo_base_value"]
+                other_sheet_elo = elo_dict.get(other_name)
+                if other_sheet_elo : 
+                    other_elo = other_sheet_elo
+                new_elo += sheet_details["elo_k_factor"] * (win_status - 1/(1+pow(10, (other_elo-player_elo)/400)))
+        new_elo = round(new_elo)
+        new_elo_dict[player_name] = new_elo
+        print(player_name, ": Old ELO", player_elo, "| new ELO", new_elo)
 
 # Send the new values to the sheet        
 for player in new_elo_dict :
