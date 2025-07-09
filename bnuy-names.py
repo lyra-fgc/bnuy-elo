83d90d79e701e7b1007cf4b00469d5105d5eedf1
Lyra <>
2025-07-09 21:51:47 +0200

    bnuy-names challonge.com + start.gg support

--- bnuy-names.py
+++ bnuy-names.py
@@ -1,12 +1,18 @@
 import json
 import challonge 
 import gspread
+import helpers
+import startgg
 
 # Challonge setup
 challonge_credentials = json.load(open("challonge_credentials.json"))
 username = challonge_credentials["username"]
 private_key = challonge_credentials["private_key"]
 challonge.set_credentials(username, private_key)
+
+# start.gg setup
+startgg_credentials = json.load(open("startgg_credentials.json"))
+startgg.set_access_token(startgg_credentials["access_token"])
 
 # Google Sheets setup
 sheet_details = json.load(open("sheet_details.json"))
@@ -17,22 +23,50 @@ names_ws = sh.worksheet(sheet_details["player_names_worksheet"])
 names_list = names_ws.get_all_records()
 print("Preparing to update player names, there are currently", len(names_list), "players on the list")
 
-tournament_name = input("Enter tournament ID (e.g. 10230) or URL (e.g. 'single_elim' for challonge.com/single_elim).\nIf assigned to a subdomain, URL format must be :subdomain-:tournament_url (e.g. 'test-mytourney' for test.challonge.com/mytourney)\n")
-tournament = challonge.tournaments.show(tournament_name)
-participants = challonge.participants.index(tournament["id"])
+(tournament_platform, tournament_name) = helpers.read_tournament_url()
 
-username_dict = {}
+if tournament_platform == "challonge.com" :
+    tournament = challonge.tournaments.show(tournament_name)
+    participants = challonge.participants.index(tournament["id"])
+    users = [{"id": p["challonge_user_id"], "name": p["username"]} for p in participants]
+    existing_ids = {g["Challonge_ID"] for g in names_list}
+    def check_similar(line, user_name) :
+        return line["StartGG_GamerTag"].casefold() == user_name.casefold()
+    def get_sheet_values(user, existing_line = {}) :
+        return [[user["id"], user["name"],
+                 existing_line.get("StartGG_ID", ''), existing_line.get("StartGG_GamerTag", ''),
+                 existing_line.get("Player_Name", user["name"])]]
 
-for p in participants :
-    username_dict[p["challonge_user_id"]] = p["username"]
+elif tournament_platform == "www.start.gg" :
+    players = startgg.get_players(tournament_name)
+    users = [{"id": p["id"], "name": p["gamerTag"]} for p in players]
+    existing_ids = {g["StartGG_ID"] for g in names_list}
+    def check_similar(line, user_name) :
+        return line["Challonge_Name"].casefold() == user_name.casefold()
+    def get_sheet_values(user, existing_line = {}) :
+        return [[existing_line.get("Challonge_ID", ''), existing_line.get("Challonge_Name", ''),
+                 user["id"], user["name"],
+                 existing_line.get("Player_Name", user["name"])]]
+
+else :
+    raise ValueError(f"Unsupported platform: {tournament_platform}")
 
 print("Users that participated in this tournament were :")
-print(username_dict.values())
+print([u["name"] for u in users])
 
-for user_id in username_dict : 
-    tmp = [l for l in names_list if l["Challonge_ID"] == user_id]
-    if len(tmp) == 0 :
-        print("Adding new user", user_id, username_dict[user_id])
-        names_ws.append_rows(values=[[user_id, username_dict[user_id], username_dict[user_id]]])
+missing = [u for u in users if u["id"] not in existing_ids]
+for user in missing :
+    similar = [l for l in names_list if check_similar(l, user["name"])]
+    if len(similar) == 1 :
+        [to_update] = similar
+        player_name = to_update["Player_Name"]
+        row = names_ws.find(player_name).row
+        values = get_sheet_values(user, to_update)
+        print("Merging into existing user", values)
+        names_ws.update(values, f"A{row}:E{row}")
+    else :
+        values = get_sheet_values(user)
+        print("Adding new user", values)
+        names_ws.append_rows(values=values)
         
 print("Player names updated, there is now", len(names_ws.get_all_records()), "players on the list")
