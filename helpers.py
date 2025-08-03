b86422a886a8238bd2b45b6747181c79952b3fdb
Lyra <>
2025-08-03 23:28:17 +0200

    modify tournament URL regex to make "https://" optional

--- helpers.py
+++ helpers.py
@@ -3,7 +3,7 @@ import re
 def read_tournament_url():
     prompt = "Enter tournament URL (e.g. 'https://challonge.com/single_elim' or 'https://www.start.gg/tournament/les-veaux-bordelais-2025')\n"
     tournament_url = input(prompt).strip()
-    m = re.match(r"https://(www\.start\.gg|(?:([^.]+)\.)?challonge\.com)/(?:tournament/)?([\w\.-]+)/?\w*", tournament_url)
+    m = re.match(r"(?:https://)(www\.start\.gg|(?:([^.]+)\.)?challonge\.com)/(?:tournament/)?([\w\.-]+)/?\w*", tournament_url)
     if m:
         tournament_platform = m.group(1)
         tournament_name = m.group(3)
